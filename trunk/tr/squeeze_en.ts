32bc0dfb34bc67c72471cde4a9c4aea25a0b514e
me@sebruiz.net <me@sebruiz.net@aa3f5287-b73c-0410-8e6d-af91a2a05ae3>
2007-10-13 00:09:53 +0000

    add translation stuff

---
+++ trunk/tr/squeeze_en.ts
@@ -0,0 +1,65 @@
+<!DOCTYPE TS><TS>
+<defaultcodec></defaultcodec>
+<context>
+    <name>ImagesView</name>
+    <message>
+        <source>Actions</source>
+        <translation>Actions</translation>
+    </message>
+    <message>
+        <source>Add some images to begin</source>
+        <translation>Add some images to begin</translation>
+    </message>
+</context>
+<context>
+    <name>SqueezeWindow</name>
+    <message>
+        <source>&amp;Add Images</source>
+        <translation>&amp;Add Images</translation>
+    </message>
+    <message>
+        <source>Width</source>
+        <translation>Width</translation>
+    </message>
+    <message>
+        <source>%</source>
+        <translation>%</translation>
+    </message>
+    <message>
+        <source>Height</source>
+        <translation>Height</translation>
+    </message>
+    <message>
+        <source>&amp;Lock Aspect Ratio</source>
+        <translation>&amp;Lock Aspect Ratio</translation>
+    </message>
+    <message>
+        <source>Save Directory</source>
+        <translation>Save Directory</translation>
+    </message>
+    <message>
+        <source>&amp;Overwrite files</source>
+        <translation>&amp;Overwrite files</translation>
+    </message>
+    <message>
+        <source>File Suffix</source>
+        <translation>File Suffix</translation>
+    </message>
+    <message>
+        <source>&amp;Resize Images</source>
+        <translation>&amp;Resize Images</translation>
+    </message>
+    <message>
+        <source>Select Images</source>
+        <translation>Select Images</translation>
+    </message>
+    <message>
+        <source>Images (*.png *.jpg *.jpeg *.bmp)</source>
+        <translation>Images (*.png *.jpg *.jpeg *.bmp)</translation>
+    </message>
+    <message>
+        <source>Select Save Directory</source>
+        <translation>Select Save Directory</translation>
+    </message>
+</context>
+</TS>
