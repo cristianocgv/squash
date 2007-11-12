9710269e2323bd784f5c6419a331ee35125bee44
me@sebruiz.net <me@sebruiz.net@aa3f5287-b73c-0410-8e6d-af91a2a05ae3>
2007-11-12 23:09:24 +0000

    Add polish translation courtesy of Grzegorz Gibas.

---
+++ trunk/tr/squash_pl.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="pl_PL">
+<defaultcodec></defaultcodec>
+<context>
+    <name>ImagesView</name>
+    <message>
+        <location filename="" line="1852402735"/>
+        <source>Actions</source>
+        <translation>Akcje</translation>
+    </message>
+    <message>
+        <location filename="" line="1852402735"/>
+        <source>Add some images to begin</source>
+        <translation>Dodaj kilka obrazków żeby rozpocząć</translation>
+    </message>
+</context>
+<context>
+    <name>SqueezeWindow</name>
+    <message>
+        <location filename="" line="1852402735"/>
+        <source>&amp;Add Images</source>
+        <translation>&amp;Dodaj obrazy</translation>
+    </message>
+    <message>
+        <location filename="" line="1852402735"/>
+        <source>Width</source>
+        <translation>Szerokość</translation>
+    </message>
+    <message>
+        <location filename="" line="1852402735"/>
+        <source>%</source>
+        <translation>%</translation>
+    </message>
+    <message>
+        <location filename="" line="1852402735"/>
+        <source>Height</source>
+        <translation>Wysokość</translation>
+    </message>
+    <message>
+        <location filename="" line="1852402735"/>
+        <source>&amp;Lock Aspect Ratio</source>
+        <translation>&amp;Zachowaj proporcje obrazu</translation>
+    </message>
+    <message>
+        <location filename="" line="1852402735"/>
+        <source>Save Directory</source>
+        <translation>Katalog zapisu</translation>
+    </message>
+    <message>
+        <location filename="" line="1852402735"/>
+        <source>&amp;Overwrite files</source>
+        <translation>&amp;Nadpisz pliki</translation>
+    </message>
+    <message>
+        <location filename="" line="1852402735"/>
+        <source>File Suffix</source>
+        <translation>Prefiks pliku</translation>
+    </message>
+    <message>
+        <location filename="" line="1852402735"/>
+        <source>&amp;Resize Images</source>
+        <translation>&amp;Zmień rozmiar obrazków</translation>
+    </message>
+    <message>
+        <location filename="" line="1852402735"/>
+        <source>Select Images</source>
+        <translation>Wybierz obrazki</translation>
+    </message>
+    <message>
+        <location filename="" line="1852402735"/>
+        <source>Images (*.png *.jpg *.jpeg *.bmp)</source>
+        <translation>Obrazy (*.png *.jpg *.jpeg *.bmp)</translation>
+    </message>
+    <message>
+        <location filename="" line="1852402735"/>
+        <source>Select Save Directory</source>
+        <translation>Wybierz katalog do zapisu</translation>
+    </message>
+</context>
+</TS>
